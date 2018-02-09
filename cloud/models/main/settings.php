d173e2bd9a2da20b96e6ffffcb36963182b7084f
wd1 <akmksi211@gmail.com>
2018-02-09 10:14:58 -0500

    Change credential

--- cloud/models/main/settings.php
+++ cloud/models/main/settings.php
@@ -1,9 +1,9 @@
 <?php
 //your username here for MYSQL
-$username= "adminuxEzwbl";
+$username= "adminZTi4jHd";
 
 //your password here for MYSQL
-$dbPassword = "Kw9CTRC7b7fx";
+$dbPassword = "NVtX3DhNAIQX";
 
 //keep database as BTC
 $database= "btcrecords";
